7b811884ebfd64ddd7a84e3926f050950fa8b81d
manngold <jsw4820@gmail.com>
2018-09-17 21:38:02 +0900

    sugar carry

--- sugar-carry.c
+++ sugar-carry.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int a;
-	printf("값을 입력하세요: ");
-	scanf("%d", &a);
-	printf("입력한 값 %d", a);
+	int sugar;
+	int move = 0;
+	scanf("%d", &sugar);
+	sugarCarry(sugar, move);
 	return 0;
 }
+
+int sugarCarry(int sugar, int move) {
+	sugar == 0 ? printf("%d", move) : (sugar % 5) == 0 ? sugarCarry(0, move+(sugar/5)) : (sugar % 3) == 0 || (sugar % 3) == 1 || (sugar % 3) == 2 ? sugarCarry(sugar - 3, ++move) : printf("-1");
+};
+
