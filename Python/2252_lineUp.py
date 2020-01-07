d2c504fdf10494d12b6ce3cff5ce13d781c5e49a
Manngold <jsw4820@gmail.com>
2020-01-07 19:18:03 +0900

    200107 Algorithm study record

--- Python/2252_lineUp.py
+++ Python/2252_lineUp.py
@@ -1,29 +1,30 @@
 n, m = map(int, input().split())
-
-q = []
-
-cnt = [0] * n
 compare = []
+studentList = [[] for i in range(n + 1)]
+cnt = [0] * (n + 1)
+q = []
 answer = []
 
 for i in range(m):
-    a, b = map(int, input().split())
-    cnt[b-1] += 1
-    compare.append([a, b])
+    l = list(map(int, input().split()))
+    compare.append(l)
 
-for i in range(n):
+for [i, j] in compare:
+    studentList[i].append(j)
+    cnt[j] += 1
+
+for i in range(1, n + 1):
     if cnt[i] == 0:
-        q.append(i + 1)
+        q.append(i)
 
 while q:
     for i in q:
         temp = i
         q.remove(i)
         answer.append(temp)
-        for j in range(len(compare)):
-            a, b = compare[j][0], compare[j][1]
-            cnt[b-1] -= 1
-            if cnt[b-1] == 0:
-                q.append(b)
+        for j in studentList[temp]:
+            cnt[j] -= 1
+            if cnt[j] == 0:
+                q.append(j)
 
-print(answer)
+print(" ".join(map(str, answer)))
