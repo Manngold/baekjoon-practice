7ae200af2ca4dcbefb3f6b5be92fd1de9a308e54
Manngold <jsw4820@gmail.com>
2020-02-27 20:38:22 +0900

    200227 Algorithm study record

--- Python/1932_intTriangle.py
+++ Python/1932_intTriangle.py
@@ -10,10 +10,8 @@ for i in range(1, n):
     for j in range(i):
         if j == 0:
             dp[i-1][j] = arr[i][j] + arr[i-1][j]
-            print(dp)
         elif j == i:
             dp[i-1][j] = arr[i][j] + arr[i-1][j-1]
-            print(dp)
         else:
             if arr[i][j] + arr[i-1][j] > dp[i-1][j]:
                 dp[i-1][j] = arr[i][j] + arr[i-1][j]
