db2d9214513f338a87d3e5c76e3b21cd2a6ccc4f
Manngold <jsw4820@gmail.com>
2020-10-02 18:02:35 +0900

    201002 Algorithm Study Record

--- Python/2583_GetArea.py
+++ Python/2583_GetArea.py
@@ -3,6 +3,7 @@ from collections import deque
 m, n, k = map(int, input().split())
 
 arr = [[0 for _ in range(n)] for _ in range(m)]
+checker = [[False for _ in range(n)] for _ in range(m)]
 answer = []
 
 for _ in range(k):
@@ -11,39 +12,33 @@ for _ in range(k):
     for x in range(x0, x1):
       if arr[y][x] == 0:
         arr[y][x] += 1
+        checker[y][x] = True
 
-def dfs(arr, x, y):
-  cnt = 0
+def dfs(y, x):
+  count = 0
   q = deque()
-  q.append([x,y])
+  q.append([x, y])
 
   dx = [0, 1, 0, -1]
   dy = [1, 0, -1, 0]
-
   while q:
     x, y = q.popleft()
-    print(y, x)
-    cnt += 1
-    arr[y][x] = 1
-
+    if checker[y][x] == False and arr[y][x] == 0:
+      checker[y][x] = True
+      arr[y][x] = 1
+      count += 1
+    else:
+      continue
     for i in range(4):
-      if y + dy[i] > 0 and x + dx[i] > 0 and x + dx[i] < n and y + dy[i] < m:
-        if arr[y + dy[i]][x + dx[i]] == 0:
-          q.append([y+dy[i], x+dx[i]])
+      if x + dx[i] >= 0 and y + dy[i] >= 0 and x+dx[i] < n and y + dy[i] < m:
+        if arr[y + dy[i]][x+dx[i]] == 0 and checker[y + dy[i]][x+dx[i]] == False:
+          q.append([x+dx[i],y+dy[i]])
+  return count
 
-  return cnt
+for y in range(m):
+  for x in range(n):
+    if arr[y][x] == 0 and checker[y][x] == False:
+      answer.append(dfs(y, x))
 
-for i in range(m):
-  for j in range(n):
-    if arr[i][j] == 0:
-      answer.append(dfs(arr, j, i))
-
-print(answer)
-
-# [
-#   [0, 0, 0, 0, 1, 1, 0],
-#   [0, 1, 0, 0, 1, 1, 0],
-#   [1, 1, 1, 1, 0, 0, 0],
-#   [1, 1, 1, 1, 0, 0, 0],
-#   [0, 1, 0, 0, 0, 0, 0]
-#   ]
+print(len(answer))
+print(" ".join(map(str, sorted(answer))))
